c6525e4b5e682d61381ce9a4c04484d2748dca13
asafked121 <72229167+asafked121@users.noreply.github.com>
2025-10-29 12:51:01 -0700

    try agian

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "<h1>￿ Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080) 
